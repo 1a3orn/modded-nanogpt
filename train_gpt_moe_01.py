99f1bd90d9c6267c3a56612c2adf183844e5319c
1a3orn <1a3orn@protonmail.com>
2025-01-25 16:18:14 -0500

    ASD

--- train_gpt_moe_01.py
+++ train_gpt_moe_01.py
@@ -527,7 +527,7 @@ class Hyperparameters:
     # evaluation and logging
     val_loss_every = 125 # every how many steps to evaluate val loss? 0 for only at the end
     # implementation
-    seq_len = 64*1024 / scale_factor # FlexAttention sequence length
+    seq_len = 64*1024 // scale_factor # FlexAttention sequence length
     save_checkpoint = False
 args = Hyperparameters()
 
