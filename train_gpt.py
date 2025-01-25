035f68bd7b09b94e6f30d7f2b833de476dd2a5be
1a3orn <1a3orn@protonmail.com>
2025-01-25 07:15:18 -0500

    Change.

--- train_gpt.py
+++ train_gpt.py
@@ -402,7 +402,7 @@ class ValueEmbedding(nn.Module):
     def forward(self, input_seq) -> list[Tensor | None]:
         ve = [emb(input_seq) for emb in self.embed]
         # 012 ... 012 structure on token value embeddings by @YouJiacheng, improved on @leloykun's U-net structure
-        ve = [ve[0], ve[1], ve[2], None, None, None, None, None, None, ve[0], ve[1], ve[2]]
+        ve = [ve[0], ve[1], None, None, None, None, None, None, ve[0], ve[1]]
         return ve
 
 # -----------------------------------------------------------------------------
@@ -592,8 +592,8 @@ print0("="*100)
 # load data
 train_loader = distributed_data_generator(args.train_files, args.batch_size, rank, world_size)
 
-layer_defs = ["mlp"] * 4 + ["moe_com"] * 4 + ["mlp"] * 4
-model = GPT(vocab_size=50257, num_layers=12, num_heads=6, model_dim=768, layer_definitions=layer_defs).cuda()
+layer_defs = ["mlp"] * 2 + ["moe_com"] * 6 + ["mlp"] * 2
+model = GPT(vocab_size=50257, num_layers=10, num_heads=6, model_dim=768, layer_definitions=layer_defs).cuda()
 for m in model.modules():
     if isinstance(m, nn.Embedding):
         m.bfloat16()
