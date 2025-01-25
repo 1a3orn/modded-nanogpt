e92e0a5f1dde03d55f0d3f05042289fae5de1345
1a3orn <1a3orn@protonmail.com>
2025-01-24 20:26:50 -0500

    Change.

--- train_gpt.py
+++ train_gpt.py
@@ -592,7 +592,7 @@ print0("="*100)
 # load data
 train_loader = distributed_data_generator(args.train_files, args.batch_size, rank, world_size)
 
-layer_defs = ["mlp"] * 4 + ["mlp_moe_com"] * 4 + ["mlp"] * 4
+layer_defs = ["mlp"] * 4 + ["moe_com"] * 4 + ["mlp"] * 4
 model = GPT(vocab_size=50257, num_layers=12, num_heads=6, model_dim=768, layer_definitions=layer_defs).cuda()
 for m in model.modules():
     if isinstance(m, nn.Embedding):
