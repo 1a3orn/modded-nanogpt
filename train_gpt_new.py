bfabbcb81e41ccfd2bccaa28220cf4f7a22f8fb8
1a3orn <1a3orn@protonmail.com>
2025-03-15 14:47:00 -0400

    ASD

--- train_gpt_new.py
+++ train_gpt_new.py
@@ -475,7 +475,7 @@ if master_process:
     os.makedirs("logs", exist_ok=True)
     logfile = f"logs/{run_id}.txt"
     print(logfile)
-    wandb.init(project="nanogpt-base", config=args, name="base")
+    wandb.init(project="nanogpt_experiment", config=args, name="base")
 def print0(s, console=False):
     if master_process:
         with open(logfile, "a") as f:
